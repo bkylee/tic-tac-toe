3ac548124466cdf2991c0462a266ae01c755ff6d
bkylee <brian.ky.lee@outlook.com>
2022-08-12 17:33:46 -0400

    Created module for gameboard

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const gameboard = (() => {
+    const board = [];
     const div1 = document.createElement('div');
+    board.push(div1);
     const div2 = document.createElement('div');
+    board.push(div2);
     const div3 = document.createElement('div');
+    board.push(div3);
     const div4 = document.createElement('div');
+    board.push(div4);
     const div5 = document.createElement('div');
+    board.push(div5);
     const div6 = document.createElement('div');
+    board.push(div6);
     const div7 = document.createElement('div');
+    board.push(div7);
     const div8 = document.createElement('div');
+    board.push(div8);
     const div9 = document.createElement('div');
+    board.push(div9);
+return {
+    board
+}
+})();
+
+const players = () =>{
+
+}
+
+
+const game = (()=>{
+    const check = () => {
+        //check board to see if there's 3 in a row
+    };
+
 })();
