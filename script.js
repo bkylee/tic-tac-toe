4f081b8c124236238efb458d983f3554b7aeaeb1
bkylee <brian.ky.lee@outlook.com>
2022-08-12 17:55:37 -0400

    created conditions to check win

--- script.js
+++ script.js
@@ -23,14 +23,59 @@ return {
 }
 })();
 
-const players = () =>{
+const players = (sign) =>{
+    this.sign = sign;
+};
 
-}
 
-
-const game = (()=>{
-    const check = () => {
-        //check board to see if there's 3 in a row
+const game = ()=>{
+    const win = ()=>{
+        //win screen whatever 
+    }
     };
+
+    const checkWin = (sign) => {
+        //check board to see if there's 3 in a row
+        if (div1.textContent === sign && div2.textContent === sign && div3.textContent === sign){
+            win();
+        }
+        else if (div4.textContent === sign && div5.textContent === sign && div6.textContent === sign){
+            win();
+        }
+        else if (div7.textContent === sign && div8.textContent === sign && div9.textContent === sign){
+            win();
+        }
+        //left-most column 3 in a row 
+        else if (div1.textContent === sign && div4.textContent === sign && div7.textContent === sign){
+            win();
+        }
+        //middle column
+        else if (div2.textContent === sign && div5.textContent === sign && div8.textContent === sign){
+            win();
+        }
+        //right-most column
+        else if (div3.textContent === sign && div6.textContent === sign && div9.textContent === sign){
+            win();
+        }
+        // diagonal 1 
+        else if (div7.textContent === sign && div5.textContent === sign && div3.textContent === sign){
+            win();
+        }
+        //diagonal 2
+        else if (div9.textContent === sign && div5.textContent === sign && div1.textContent === sign){
+            win(); 
+        }
+    };
+    const checkBlank = () => {
+        //check white spaces 
+    };
+
+    const CPUmove = () => {
+        //based on white spaces, CPU does their move
+    };
+
+    return{
+        checkWin, checkBlank, CPUmove
+    }
 
 })();
