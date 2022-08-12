083ee1e2c2ee6b6c1903d85790764722eb433fe0
bkylee <brian.ky.lee@outlook.com>
2022-08-12 19:08:40 -0400

    edited some calls of checkBlank

--- script.js
+++ script.js
@@ -2,29 +2,40 @@ const gameboard = (() => {
     const board = [];
     const div1 = document.getElementById('1');
     board.push(div1);
-    const div2 = document.getElementById(2);
+    const div2 = document.getElementById('2');
     board.push(div2);
-    const div3 = document.getElementById(3);
+    const div3 = document.getElementById('3');
     board.push(div3);
-    const div4 = document.getElementById(4);
+    const div4 = document.getElementById('4');
     board.push(div4);
-    const div5 = document.getElementById(5);
+    const div5 = document.getElementById('5');
     board.push(div5);
-    const div6 = document.getElementById(6);
+    const div6 = document.getElementById('6');
     board.push(div6);
-    const div7 = document.getElementById(7);
+    const div7 = document.getElementById('7');
     board.push(div7);
-    const div8 = document.getElementById(8);
+    const div8 = document.getElementById('8');
     board.push(div8);
-    const div9 = document.getElementById(9);
+    const div9 = document.getElementById('9');
     board.push(div9);
 return {
     board
 }
 })();
 
+const checkBlank = (board)=> {
+    blanks = [];
+    board.array.forEach(element => {
+        if (element.textContent === ""){
+            blanks.push(element);
+        } 
+    });
+    return {board}
+};
+
 const players = (sign) =>{
     this.sign = sign;
+    blanks = checkBlank.board;
 };
 
 
@@ -67,20 +78,10 @@ const game = ()=>{
         }
     };
 
-    const checkBlank = (board)=> {
-        blanks = [];
-        board.array.forEach(element => {
-            if (element.textContent === ""){
-                blanks.push(element);
-            } 
-        });
-        return {board}
-    };
-
     const CPUmove = () => {
         //based on white spaces, CPU does their move
-        blanks = checkblank
-        move = math.floor(Math.random()*)
+        blanks = checkBlank.board;
+        move = math.floor(Math.random()*blanks.length)
     }
 
     return{
