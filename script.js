86bba54c6304dd2f9ac3b59de988f2cf0a15e063
bkylee <brian.ky.lee@outlook.com>
2022-08-12 17:26:34 -0400

    created gameboard module and random stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+const gameboard = (() => {
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    const div4 = document.createElement('div');
+    const div5 = document.createElement('div');
+    const div6 = document.createElement('div');
+    const div7 = document.createElement('div');
+    const div8 = document.createElement('div');
+    const div9 = document.createElement('div');
+})();
