6658d8be4cfa3ad696a4c66fcbacb5b879ce58f7
bkylee <brian.ky.lee@outlook.com>
2022-08-14 14:48:12 -0400

    Added event listener to divs for user to click and add x

--- script.js
+++ script.js
@@ -18,11 +18,18 @@ const gameboard = (() => {
     board.push(div8);
     const div9 = document.getElementById('9');
     board.push(div9);
+    board.forEach((element)=>{
+        addEventListener('click', ()=>{
+            if (element.textContent === ""){
+            element.textContent = 'x';}
+        })
+    });
 return {
     board
 }
 })();
 
+board
 const checkBlank = (board)=> {
     blanks = [];
     board.array.forEach(element => {
@@ -42,52 +49,51 @@ const win = ((player) =>{
     screen.textContent = `${player.sign} wins!` 
 })();
 
+const checkWin = (player) => {
+    //check board to see if there's 3 in a row
+    if (div1.textContent === player.sign && div2.textContent === player.sign && div3.textContent === player.sign){
+        win(player);
+    }
+    else if (div4.textContent === player.sign && div5.textContent === player.sign && div6.textContent === player.sign){
+        win(player);
+    }
+    else if (div7.textContent === player.sign && div8.textContent === player.sign && div9.textContent === player.sign){
+        win(player);
+    }
+    //left-most column 3 in a row 
+    else if (div1.textContent === player.sign && div4.textContent === player.sign && div7.textContent === player.sign){
+        win(player);
+    }
+    //middle column
+    else if (div2.textContent === player.sign && div5.textContent === player.sign && div8.textContent === player.sign){
+        win(player);
+    }
+    //right-most column
+    else if (div3.textContent === player.sign && div6.textContent === player.sign && div9.textContent === player.sign){
+        win(player);
+    }
+    // diagonal 1 
+    else if (div7.textContent === player.sign && div5.textContent === player.sign && div3.textContent === player.sign){
+        win(player);
+    }
+    //diagonal 2
+    else if (div9.textContent === player.sign && div5.textContent === player.sign && div1.textContent === player.sign){
+        win(player); 
+    }
+};
+
+const CPUmove = () => {
+    //based on white spaces, CPU does their move
+    blanks = checkBlank.board;
+    move = math.floor(Math.random()*blanks.length);
+return{
+    move
+}
+}
 const game = ()=>{
     const player1 = players('x');
     const player2 = players('o');
 
     board = gameboard.board;
-    board.forEach(addEventListener('click', ()=>{
-        
-    }))
+
 }
-    const checkWin = (player) => {
-        //check board to see if there's 3 in a row
-        if (div1.textContent === player.sign && div2.textContent === player.sign && div3.textContent === player.sign){
-            win(player);
-        }
-        else if (div4.textContent === player.sign && div5.textContent === player.sign && div6.textContent === player.sign){
-            win(player);
-        }
-        else if (div7.textContent === player.sign && div8.textContent === player.sign && div9.textContent === player.sign){
-            win(player);
-        }
-        //left-most column 3 in a row 
-        else if (div1.textContent === player.sign && div4.textContent === player.sign && div7.textContent === player.sign){
-            win(player);
-        }
-        //middle column
-        else if (div2.textContent === player.sign && div5.textContent === player.sign && div8.textContent === player.sign){
-            win(player);
-        }
-        //right-most column
-        else if (div3.textContent === player.sign && div6.textContent === player.sign && div9.textContent === player.sign){
-            win(player);
-        }
-        // diagonal 1 
-        else if (div7.textContent === player.sign && div5.textContent === player.sign && div3.textContent === player.sign){
-            win(player);
-        }
-        //diagonal 2
-        else if (div9.textContent === player.sign && div5.textContent === player.sign && div1.textContent === player.sign){
-            win(player); 
-        }
-    };
-
-    const CPUmove = () => {
-        //based on white spaces, CPU does their move
-        blanks = checkBlank.board;
-        move = math.floor(Math.random()*blanks.length);
-
-    }
-
